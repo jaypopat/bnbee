d71fc1ef833fcb3ebf556838052b05c2e3273d60
Mario C <cavalmario2003@gmail.com>
2024-09-30 18:01:18 +0100

    Added Review Service and Controller (#8)

--- src/main/java/com/bnbair/bnbair/repository/ReviewRepository.java
+++ src/main/java/com/bnbair/bnbair/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.bnbair.bnbair.repository;
 
 import com.bnbair.bnbair.domain.Review;
+import com.bnbair.bnbair.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends CrudRepository<Review, Long> {
+  List<Review> findAllByReviewer(User user);
 }
