d44b28c6041cfadc8bf2ec838036c793d587165f
Mario C <cavalmario2003@gmail.com>
2024-10-01 12:59:46 +0100

    Updated User.java so querying for a user's bookings, properties and reviews is simpler

--- src/main/java/com/bnbair/bnbair/domain/User.java
+++ src/main/java/com/bnbair/bnbair/domain/User.java
@@ -1,13 +1,15 @@
 package com.bnbair.bnbair.domain;
 
 //import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Setter
 @Getter
@@ -17,58 +19,69 @@ import java.time.LocalDateTime;
 @Table(name = "user_table") // Must keep this name as 'user' is reserved in H2
 public class User {
 
-    @Id
-    @Setter(AccessLevel.NONE)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @Setter(AccessLevel.NONE)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotBlank(message = "First name is required")
-    @Column(nullable = false)
-    private String firstName;
+  @NotBlank(message = "First name is required")
+  @Column(nullable = false)
+  private String firstName;
 
-    @NotBlank(message = "Last name is required")
-    @Column(nullable = false)
-    private String lastName;
+  @NotBlank(message = "Last name is required")
+  @Column(nullable = false)
+  private String lastName;
 
-    @Email(message = "Email should be valid")
-    @NotBlank(message = "Email is required")
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Email(message = "Email should be valid")
+  @NotBlank(message = "Email is required")
+  @Column(nullable = false, unique = true)
+  private String email;
 
 //    @NotBlank(message = "Password is required")
 //    @Size(min = 8, message = "Password must be at least 8 characters long")
 //    @JsonIgnore
 //    private String password;
 
+  @JsonIgnore
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "booker")
+  private List<Booking> bookings;
 
-    @Column(nullable = false, updatable = false)
-    @Setter(AccessLevel.NONE)
-    private LocalDateTime createdAt = LocalDateTime.now();
+  @JsonIgnore
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+  private List<Property> properties;
 
-    @Column
-    private Float ownerRating;
+  @JsonIgnore
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "reviewer")
+  private List<Review> reviews;
 
-    @Column
-    private Float guestRating;
+  @Column(nullable = false, updatable = false)
+  @Setter(AccessLevel.NONE)
+  private LocalDateTime createdAt = LocalDateTime.now();
 
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        ownerRating = null;
-        guestRating = null;
-    }
+  @Column
+  private Float ownerRating;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", createdAt=" + createdAt +
-                ", ownerRating=" + ownerRating +
-                ", guestRating=" + guestRating +
-                '}';
-    }
+  @Column
+  private Float guestRating;
+
+  public User(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    ownerRating = null;
+    guestRating = null;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", email='" + email + '\'' +
+            ", createdAt=" + createdAt +
+            ", ownerRating=" + ownerRating +
+            ", guestRating=" + guestRating +
+            '}';
+  }
 }
--- src/main/java/com/bnbair/bnbair/repository/BookingRepository.java
+++ src/main/java/com/bnbair/bnbair/repository/BookingRepository.java
@@ -1,11 +1,15 @@
 package com.bnbair.bnbair.repository;
 
 import com.bnbair.bnbair.domain.Booking;
+import com.bnbair.bnbair.domain.BookingStatus;
 import com.bnbair.bnbair.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends CrudRepository<Booking, Long> {
 
@@ -14,6 +18,9 @@ public interface BookingRepository extends CrudRepository<Booking, Long> {
     List<Booking> findByBooker_Id(Long id);
 
     List<Booking> findByProperty_Id(Long id);
-    List<Booking> findByProperty_IdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
-            Long propertyId, LocalDate startDate, LocalDate endDate);
+
+    List<Booking> findByStatus(BookingStatus status);
+
+    @Query("SELECT b FROM Booking b WHERE b.property.id = ?1 AND b.startDate >= ?2 AND b.endDate <= ?3")
+    List<Booking> findByPropertyIdBetween(@Param("propertyId") Long propertyId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
