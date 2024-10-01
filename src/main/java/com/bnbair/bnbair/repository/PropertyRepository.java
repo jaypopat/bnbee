fc98a9e6aee54486af9e03822a31d7b905e59f4e
Wex2k <patriks.bried@gmail.com>
2024-10-01 09:20:39 +0100

    Add customs methods for searching for properties

--- src/main/java/com/bnbair/bnbair/repository/PropertyRepository.java
+++ src/main/java/com/bnbair/bnbair/repository/PropertyRepository.java
@@ -1,8 +1,16 @@
 package com.bnbair.bnbair.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.bnbair.bnbair.domain.Property;
 
 public interface PropertyRepository extends CrudRepository<Property, Long> {
+    public List<Property> findByCountry(String country);
+
+    @Query("select p from Property p where p.pricePerNight between :low and :high")
+    List<Property> findPropertyByPriceBetween(@Param("low") float low, @Param("high") float high);
 }
