35b152386f605db805fa91e8059ef16ed7687bc7
Patriks <60120715+Wex2k@users.noreply.github.com>
2024-10-09 22:58:52 +0100

    Organized backend and frontend into seperate folders (#18)
    * Organized backend and frontend into seperate folders
    
    * Fixed eslint showing "jsx must be in scope"
    
    * Fixed eslint showing "jsx must be in scope"
    
    ---------
    
    Co-authored-by: Mario C <cavalmario2003@gmail.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-secondary flex items-center justify-center">
+        <Button className="uppercase">bnbee</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
