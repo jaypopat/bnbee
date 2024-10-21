99885fbb329c1e75f2e4e0be43089ef4180308f2
Mario Caval <cavalmario2003@gmail.com>
2024-10-21 17:22:44 +0100

    Added Sign Up and Sign In Dialog (no functionality yet)

--- frontend/src/components/AccountDropDown.tsx
+++ frontend/src/components/AccountDropDown.tsx
@@ -9,13 +9,25 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { User, Settings, LogOut, Calendar } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import {
+  Dialog,
+  DialogTrigger,
+} from '@/components/ui/dialog.tsx';
+import SignInDialog from '@/components/SignInDialog.tsx';
+import { SignUpDialog } from '@/components/SignUpDialog.tsx';
+
+// Note: Shadcn/Radix UI have issues dealing with multiple dialogs within a dropdown menu
+// props to the guy below for the workaround
+// https://github.com/shadcn-ui/ui/issues/1011#issuecomment-1930103090
+enum Dialogs { signUpDialog, signInDialog }
 
 function AccountDropDown() {
   // Used to toggle the sign in state for now
   const [signedIn, setSignedIn] = useState(false);
+  const [dialog, setDialog] = useState(Dialogs.signUpDialog);
 
   return (
-    <React.Fragment>
+    <Dialog>
       <DropdownMenu>
         <DropdownMenuTrigger>
           <User className="text-primary" />
@@ -49,19 +61,30 @@ function AccountDropDown() {
             </React.Fragment>
           ) : (
             <React.Fragment>
-              <DropdownMenuItem className="flex items-center gap-2" onClick={() => setSignedIn(true)}>
-                <User className="size-4" />
-                <span>Sign Up</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="flex items-center gap-2" onClick={() => setSignedIn(true)}>
-                <User className="size-4" />
-                <span>Sign In</span>
-              </DropdownMenuItem>
+              <DialogTrigger asChild onClick={() => setDialog(Dialogs.signUpDialog)}>
+                <DropdownMenuItem className="flex items-center gap-2" >
+                  <User className="size-4" />
+                  <span>Sign Up</span>
+                </DropdownMenuItem>
+              </DialogTrigger>
+
+              <DialogTrigger asChild onClick={() => setDialog(Dialogs.signInDialog)}>
+                <DropdownMenuItem className="flex items-center gap-2">
+                  <User className="size-4" />
+                  <span>Sign In</span>
+                </DropdownMenuItem>
+              </DialogTrigger>
+
             </React.Fragment>
           )}
         </DropdownMenuContent>
       </DropdownMenu>
-    </React.Fragment>
+
+      {dialog === Dialogs.signUpDialog
+        ? <SignUpDialog />
+        : <SignInDialog />
+      }
+    </Dialog>
   );
 }
 
