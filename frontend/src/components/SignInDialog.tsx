d6fcffbf352db87c744f2ea118988268341d031c
Mario Caval <cavalmario2003@gmail.com>
2024-10-21 23:06:03 +0100

    Added Auth Context which stores user and methods for signin in and signing out.

--- frontend/src/components/SignInDialog.tsx
+++ frontend/src/components/SignInDialog.tsx
@@ -2,8 +2,18 @@ import { DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/compon
 import { Label } from '@/components/ui/label.tsx';
 import { Input } from '@/components/ui/input.tsx';
 import { Button } from '@/components/ui/button.tsx';
+import { useState } from 'react';
+import useAuth from '@/context/AuthProvider';
 
 export default function SignInDialog() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { signIn } = useAuth();
+
+  const handleSignIn = async () => {
+    const success = await signIn(email, password);
+    console.log("Signed in success:", success);
+  }
 
   return (
     <div>
@@ -14,15 +24,15 @@ export default function SignInDialog() {
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="email" className="text-right">Email</Label>
-            <Input id="email" type="email" placeholder="Email" className="col-span-3" />
+            <Input id="email" type="email" placeholder="Email" className="col-span-3" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="password" className="text-right">Password</Label>
-            <Input id="password" className="col-span-3" placeholder="Password" />
+            <Input id="password" className="col-span-3" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Sign in</Button>
+          <Button onClick={handleSignIn}>Sign in</Button>
         </DialogFooter>
       </DialogContent>
     </div>
