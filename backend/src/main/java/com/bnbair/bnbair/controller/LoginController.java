99157d33640af0ecf94221c293bfb946c3132590
jaypopat <contact.jaypopat@gmail.com>
2024-10-22 09:10:03 +0100

    fixed cors issues and improved log in flow (#22)
    * fixed cors issues and improved log in flow
    
    * removed unused constructor parameter - userDetailsServiceImpl

--- backend/src/main/java/com/bnbair/bnbair/controller/LoginController.java
+++ backend/src/main/java/com/bnbair/bnbair/controller/LoginController.java
@@ -1,12 +1,18 @@
 package com.bnbair.bnbair.controller;
 
 import com.bnbair.bnbair.domain.AccountCredentials;
+import com.bnbair.bnbair.domain.User;
+import com.bnbair.bnbair.exception.UserNotFoundException;
 import com.bnbair.bnbair.service.JwtService;
+import com.bnbair.bnbair.service.UserDetailsServiceImpl;
+import com.bnbair.bnbair.service.UserService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,22 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final UserService userService;
 
-    public LoginController(JwtService jwtService, AuthenticationManager authenticationManager) {
+    public LoginController(JwtService jwtService, AuthenticationManager authenticationManager, UserService userService, UserDetailsServiceImpl userDetailsServiceImpl) {
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) {
+    public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) throws UserNotFoundException {
         UsernamePasswordAuthenticationToken creds = new UsernamePasswordAuthenticationToken(credentials.email(), credentials.password());
         Authentication auth = authenticationManager.authenticate(creds);
         // Generate token
         String jwts = jwtService.getToken(auth.getName());
+        User user = userService.getUserByEmail(auth.getName());
         // Build response with the generated token
         return ResponseEntity.ok()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer"+jwts)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwts)
                 .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization")
-                .build();
+                .body(user);
     }
 }
