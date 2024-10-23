e92e5d089985c770f3c27f2774200e37199247ed
Mario C <cavalmario2003@gmail.com>
2024-10-23 20:40:04 +0000

    Added axios jwt intercepters

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,13 +3,27 @@ import axios from "axios";
 // Set up default configuration. 
 const api = axios.create({
   baseURL: "http://localhost:8080",
+  withCredentials: true,
   timeout: 1000,
 })
 
-const token = localStorage.getItem('auth_token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = "Bearer " + token;
-}
+/** 
+ * Interceptor for requests sent from the application: 
+ * retrieve the Access Token from localStorage and 
+ * add it to every API request made using the axios instance.
+ */
+api.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      config.headers['Authorization'] = "Bearer " + token;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
 export * from './auth';
